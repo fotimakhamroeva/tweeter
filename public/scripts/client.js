e1e6c2bebf7daf1fc0180ea77f3b65ac12faf5fd
Fotima Khamroeva <bonu_9229@bk.ru>
2021-12-16 20:50:04 -0500

    generated jquery html by tweets data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,56 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Fake data taken from initial-tweets.json
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    $('#tweets').append(createTweetElement(tweet));
+  }
+}
+
+const createTweetElement = function(tweet) {
+    return  `<article class="tweet">
+                <section>
+                    <div class="row">
+                    <img src='${ tweet.user.avatars }'>
+                    <h4>${ tweet.user.name }</h4>
+                    </div>
+                    <h4>${ tweet.user.handle }</h4>
+                </section>
+                <section>
+                    ${ tweet.content.text }
+                </section>
+                <footer>
+                    <section>
+                    <div>${ tweet.created_at }</div>
+                    <div><img><img><img></div>
+                    </section>
+                </footer>
+                </article>`
+}
+
+$(document).ready(function(){
+    renderTweets(data);
+});
